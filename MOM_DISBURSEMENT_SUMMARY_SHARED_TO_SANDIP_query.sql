bbff4bfa28a9cb1ea8426bf0fce34fa1282deac4
Suresh <suresh.prajapati@kissht.com>
2026-04-25 13:14:49 +0530

    updated MOM disbursement summary file

--- MOM_DISBURSEMENT_SUMMARY_SHARED_TO_SANDIP_query.sql
+++ MOM_DISBURSEMENT_SUMMARY_SHARED_TO_SANDIP_query.sql
@@ -1,8 +1,8 @@
 -- Top 50 city
-create or replace table ring_trf.transient.Apr25_dec25_top_50_city as 
+create or replace table ring_trf.transient.Apr25_Mar26_top_50_city as 
 select city_name,count(loan_reference_number) as loan_count,sum(loan_amount) as loan_amount
 from mis.core.loan_tape_FY23_25
-where date(disbursement_date) between '2025-04-01' and '2025-12-31'
+where date(disbursement_date) between '2025-04-01' and '2026-03-31'
 group by city_name order by loan_count desc limit 50;
 
 
@@ -17,6 +17,7 @@ select
             WHEN IFNULL(dis.age, 0) > 45 THEN '>45 Years'
             ELSE 'Other'
         END AS Age_Bucket,
+        tenure,
         case 
             when tenure<=2 then 'ST'
             when tenure between 3 and 5 then 'MT'
@@ -43,10 +44,10 @@ select
             when INCOME_BAND >=25000 and INCOME_BAND<50000 THEN '25K to 50K'
             when INCOME_BAND >=200 and INCOME_BAND<25000 THEN '200 to 25K'
         END) as CUSTOMER_MIX_INCOME,
-        iff(CITY_NAME in (select city_name from ring_trf.transient.Apr25_dec25_top_50_city),'TOP_50_CITIES','OTHER') as CITIS_MIX,
+        iff(CITY_NAME in (select city_name from ring_trf.transient.Apr25_Mar26_top_50_city),'TOP_50_CITIES','OTHER') as CITIS_MIX,
         sum(LOAN_AMOUNT) as LOAN_AMOUNT,
         sum(LOAN_AMOUNT*ONBOOK_RATIO/100) as Onbook_Loan_Amt,   
         sum(LOAN_AMOUNT*OFFBOOK_RATIO/100) as Offboo_Loan_Amt,
         count(loan_reference_number) as LOAN_COUNT
-from mis.core.loan_tape_FY23_25 dis where date(DISBURSEMENT_DATE) between  '2025-04-01' and '2025-12-31'
+from mis.core.loan_tape_FY23_25 dis where date(DISBURSEMENT_DATE) between  '2025-04-01' and '2026-03-31'
 group by all order by all;
